beed9f5eafb8c87213df456c0dba839e7764dd70
ameer017 <rajiabdullahi907@gmail.com>
2024-03-11 13:12:27 +0100

    voting_contract_added

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,34 +1,71 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
-
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+contract Voting {
+    struct Candidate {
+        string name;
+        uint256 voteCount;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    Candidate[] public candidates;
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+    address owner;
+    mapping(address => bool) public voters;
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+    uint256 public votingStart;
+    uint256 public votingEnd;
 
-        owner.transfer(address(this).balance);
+    constructor(string[] memory _candidateNames, uint256 _durationMinutes) {
+        for (uint256 i = 0; i < _candidateNames.length; i++) {
+            candidates.push(
+                Candidate({name: _candidateNames[i], voteCount: 0})
+            );
+        }
+        owner = msg.sender;
+        votingStart = block.timestamp;
+        votingEnd = votingStart + (_durationMinutes * 1 minutes);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function addCandidate(string memory _name) public onlyOwner {
+        candidates.push(Candidate({name: _name, voteCount: 0}));
+    }
+
+    function vote(uint256 _candidateIndex) public {
+        require(!voters[msg.sender], "You have already voted!!!");
+        require(
+            _candidateIndex < candidates.length,
+            "Candidate does not exist"
+        );
+
+        candidates[_candidateIndex].voteCount++;
+        voters[msg.sender] == true;
+    }
+
+    function getAllVotesOfCandidates()
+        public
+        view
+        returns (Candidate[] memory)
+    {
+        return candidates;
+    }
+
+    function getVotingStatus() public view returns (bool) {
+        return (block.timestamp >= votingStart && block.timestamp < votingEnd);
+    }
+
+    function getRemainingTime() public view returns (uint256) {
+        require(
+            block.timestamp >= votingStart,
+            "Voting has not started yet!!!"
+        );
+        if (block.timestamp >= votingEnd) {
+            return 0;
+        }
+        return votingEnd - block.timestamp;
     }
 }
